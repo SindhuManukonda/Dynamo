189f0d06c386b6b107d1f0334682d171afb7344c
Maurani <S1048861@monmouth.edu>
2016-03-30 23:09:11 -0400

    Added

---
+++ Dynamo/src/net/dynamo/VO/Responder.java
@@ -0,0 +1,42 @@
+package net.dynamo.VO;
+
+public class Responder {
+private String resName;
+public String getResName() {
+	return resName;
+}
+public void setResName(String resName) {
+	this.resName = resName;
+}
+private String organization;
+private String lattitude;
+private String longitude;
+private String imageName;
+
+
+public String getOrganization() {
+	return organization;
+}
+public void setOrganization(String organization) {
+	this.organization = organization;
+}
+public String getLattitude() {
+	return lattitude;
+}
+public void setLattitude(String lattitude) {
+	this.lattitude = lattitude;
+}
+public String getLongitude() {
+	return longitude;
+}
+public void setLongitude(String longitude) {
+	this.longitude = longitude;
+}
+public String getImageName() {
+	return imageName;
+}
+public void setImageName(String imageName) {
+	this.imageName = imageName;
+}
+
+}
---
+++ Dynamo/src/net/dynamo/action/FetchMapAction.java
@@ -0,0 +1,53 @@
+package net.dynamo.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.dynamo.VO.Responder;
+
+public class FetchMapAction {
+	//private static final long serialVersionUID = -6765991741441442190L;
+	public List responderDetailsList = new ArrayList();
+	
+	
+	public List getResponderDetailsList() {
+		return responderDetailsList;
+	}
+
+
+	public void setResponderDetailsList(List responderDetailsList) {
+		this.responderDetailsList = responderDetailsList;
+	}
+
+
+	/*****************
+	 * To Fetch Location of First Responder to MAP
+	 ************************/
+	public String fetchToMap() throws Exception {
+		Responder responder = new Responder();
+		responder.setResName("maurani");
+		responder.setImageName("icon.png");
+		responder.setOrganization("Fire");
+		responder.setLattitude("-33.923036");
+		responder.setLongitude("151.259052");
+		responderDetailsList.add(responder);
+		// outerList.add(responderDetailsList);
+
+		Responder responder1 = new Responder();
+		responder1.setResName("Ramya");
+		responder1.setImageName("icon.png");
+		responder1.setOrganization("Police");
+		responder1.setLattitude("-34.028249");
+		responder1.setLongitude("151.157507");
+		responderDetailsList.add(responder1);
+		// outerList.add(responderDetailsList);
+		System.out.println("responder::" + responderDetailsList);
+		// Gson m = new Gson();
+		// String jsonData = new Gson().toJson(responderDetailsList);
+		// System.out.println("jsonData::"+jsonData);
+		System.out.println("responder::" + responderDetailsList);
+
+		return "fetchToMap";
+	}
+	
+}
